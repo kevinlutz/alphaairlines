c32beb98d5637558bd4f64e7da74517e7f61b5dc
+ <+>
2021-11-23 13:50:53 -0500

    created frontend react components

---
+++ client/src/components/LogCard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LogCard() {
+  return <div></div>;
+}
---
+++ client/src/components/LogsContainer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LogsContainer() {
+  return <div></div>;
+}
